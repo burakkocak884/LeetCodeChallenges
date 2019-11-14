d11fe7834aaff0cec65e54641e842189df5867e8
Burak Kocak <burakkocak884@hotmail.com>
2019-11-14 12:31:53 -0500

    linked list

--- 387FirstUniqueCharacterInaString.js
+++ 387FirstUniqueCharacterInaString.js
@@ -11,16 +11,17 @@ s = "loveleetcode",
 return 2.
 */
 
-        var firstUniqChar = function(s) {
-                    let indexNum = 0;
-                    let theFirst = s.split("")
-                    let hash ={}
-                    for (let i = 0 ; i < s.length; i++){
-                        if(hash[s[i]]){
-                            hash[s[i]] += 1
-                        }else{
-                            hash[s[i]] = 1
-                        }
-                   }
-                return s.indexOf((Object.keys(hash).find(key =>hash[key] === 1)))
-         };
+     var firstUniqChar = function(s) {
+                
+            let hash ={}
+            for (let i = 0 ; i < s.length; i++){
+                if(hash[s[i]]){
+                    hash[s[i]] += 1
+                }else{
+                    hash[s[i]] = 1
+                }
+            }
+     return s.indexOf((Object.keys(hash).find(key =>hash[key] === 1)))
+                
+                
+            };
